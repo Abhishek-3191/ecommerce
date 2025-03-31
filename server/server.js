4a5ca761904c209d83853d502ce836b21a978f9e
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-03-31 21:13:47 +0530

    Initial setup with client and server

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const cookieParser=require("cookie-parser");
+const cors=require("cors");
+
+mongoose.connect(
+    "mongodb+srv://Abhishek:Abhi3191@cluster0.yihhzpx.mongodb.net/"
+).then(()=>{
+    console.log("Mongodb connected");
+})
+.catch((err)=>{
+    console.log("Mongodb doesnt connected",err);
+})
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin:"http://localhost:5173/",
+        method:['GET','POST','PUT','DELETE'],
+        allowedHeaders:[
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma',
+        ],
+        credentials:true,
+    })
+)
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
