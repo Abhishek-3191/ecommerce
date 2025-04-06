e1e9100f92d932e206e8d587a7e3dc6a368390ad
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-04-06 18:37:01 +0530

    store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './auth-slice';
+import authReducer from "./auth-slice";
+import adminProductsSlice from "./admin/products-slice";
+import commonFeatureSlice from "./common-slice";
+import adminOrderSlice from "./admin/order-slice"
 
-
-const store=configureStore({
-    reducer:{
-        auth:authReducer,
+const store = configureStore({
+    reducer: {
+      auth: authReducer,
+  
+      adminProducts: adminProductsSlice,
+      adminOrder: adminOrderSlice,
+  
+      commonFeature: commonFeatureSlice,
     },
-}) 
+  });
+  
 
 export default store;
