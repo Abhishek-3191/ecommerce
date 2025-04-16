ba0b7d98df1dfc942a0e018b943b96ab822175eb
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-04-16 18:44:30 +0530

    address completed

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const dburl=process.env.MONGODB_URL;
 const adminProductsRouter=require("./routes/admin/products-routes");
 const shopProductsRouter=require("./routes/shop/products-routes");
 const shopCartRouter=require("./routes/shop/cart-routes")
-
+const shopAddressRouter=require("./routes/shop/address-routes");
 
 mongoose.connect(
     dburl
@@ -42,6 +42,7 @@ app.use('/api/auth',authRouter);
 app.use('/api/admin/products',adminProductsRouter);
 app.use('/api/shop/products',shopProductsRouter);
 app.use('/api/shop/cart',shopCartRouter);
+app.use('/api/shop/address',shopAddressRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
