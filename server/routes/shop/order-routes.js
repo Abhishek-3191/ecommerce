77bbd3b5aac3d1ec6c2b7f29846d6712aa6dcf14
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-04-20 00:23:40 +0530

    routes

--- server/routes/shop/order-routes.js
+++ server/routes/shop/order-routes.js
@@ -1,8 +1,17 @@
-const express=require('express');
-const router=express.Router();
-const {capturePayment,createOrder}=require('../../controllers/shop/order-controller');
+const express = require("express");
 
-router.post('/capture',capturePayment);
-router.post('/create',createOrder);
+const {
+  createOrder,
+  getAllOrdersByUser,
+  getOrderDetails,
+  capturePayment,
+} = require("../../controllers/shop/order-controller");
 
-module.exports=router;
+const router = express.Router();
+
+router.post("/create", createOrder);
+router.post("/capture", capturePayment);
+router.get("/list/:userId", getAllOrdersByUser);
+router.get("/details/:id", getOrderDetails);
+
+module.exports = router;
