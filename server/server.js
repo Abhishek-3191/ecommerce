c195cad66f406db78a961eac09a4f383d63795f5
Abhishek-3191 <abhisheksrivastava49631@gmail.com>
2025-04-26 23:45:31 +0530

    server

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const shopProductsRouter=require("./routes/shop/products-routes");
 const shopCartRouter=require("./routes/shop/cart-routes")
 const shopAddressRouter=require("./routes/shop/address-routes");
 const shopOrderRouter=require("./routes/shop/order-routes");
+const shopSearchRouter=require("./routes/shop/search-routes");
+const shopReviewRouter=require("./routes/shop/review-routes");
+
 
 mongoose.connect(
     dburl
@@ -50,7 +53,8 @@ app.use('/api/shop/products',shopProductsRouter);
 app.use('/api/shop/cart',shopCartRouter);
 app.use('/api/shop/address',shopAddressRouter);
 app.use('/api/shop/order',shopOrderRouter);
-
+app.use('/api/shop/search',shopSearchRouter);
+app.use('/api/shop/review',shopReviewRouter);
 
 
 app.listen(PORT,()=>{
